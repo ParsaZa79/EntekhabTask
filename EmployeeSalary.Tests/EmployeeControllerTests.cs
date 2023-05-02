e04a8ac43b6d2c3828d0eaf34e4a9f6dd514c45b
Parsa Zeinali <pzeinali4@gmail.com>
2023-05-02 15:28:29 +0330

    Added tests for Update action method

--- EmployeeSalary.Tests/EmployeeControllerTests.cs
+++ EmployeeSalary.Tests/EmployeeControllerTests.cs
@@ -166,5 +166,119 @@ public class EmployeeControllerTests
         Assert.NotNull(response.Data);
     }
 
+    
+    [Fact]
+    public async Task UpdateEmployee_InvalidRequest_ReturnsBadRequest()
+    {
+        // Arrange
+        var employeeServiceMock = new Mock<IEmployeeService>();
+        var employeeSalaryServiceMock = new Mock<IEmployeeSalaryService>();
+        var updateEmployeeRequest = new UpdateEmployeeRequest();
+        var controller = new EmployeeController(employeeServiceMock.Object, employeeSalaryServiceMock.Object);
+        controller.ModelState.AddModelError("Employee", "Employee data is required");
+
+        // Act
+        var result = await controller.Update(updateEmployeeRequest);
+
+        // Assert
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+    
+    [Fact]
+    public async Task UpdateEmployee_EmployeeNotFound_ReturnsNotFound()
+    {
+        // Arrange
+        var employeeServiceMock = new Mock<IEmployeeService>();
+        var employeeSalaryServiceMock = new Mock<IEmployeeSalaryService>();
+        var updateEmployeeRequest = new UpdateEmployeeRequest
+        {
+            Employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Jane",
+                LastName = "Doe",
+                BasicSalary = 2900000,
+                Allowance = 30000,
+                Transportation = 25000
+            }
+        };
+        employeeServiceMock.Setup(es => es.UpdateEmployee(updateEmployeeRequest))
+            .ReturnsAsync(false);
+
+        var controller = new EmployeeController(employeeServiceMock.Object, employeeSalaryServiceMock.Object);
+
+        // Act
+        var result = await controller.Update(updateEmployeeRequest);
+
+        // Assert
+        var statusCodeResult = Assert.IsType<StatusCodeResult>(result);
+        Assert.Equal(500, statusCodeResult.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateEmployee_ExceptionDuringUpdate_ReturnsInternalServerError()
+    {
+        // Arrange
+        var employeeServiceMock = new Mock<IEmployeeService>();
+        var employeeSalaryServiceMock = new Mock<IEmployeeSalaryService>();
+        var updateEmployeeRequest = new UpdateEmployeeRequest
+        {
+            Employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Jane",
+                LastName = "Doe",
+                BasicSalary = 2900000,
+                Allowance = 30000,
+                Transportation = 25000
+            }
+        };
+        employeeServiceMock.Setup(es => es.UpdateEmployee(updateEmployeeRequest))
+            .ThrowsAsync(new Exception("Error updating employee"));
+
+        var controller = new EmployeeController(employeeServiceMock.Object, employeeSalaryServiceMock.Object);
+
+        // Act
+        var result = await controller.Update(updateEmployeeRequest);
+
+        // Assert
+        var statusCodeResult = Assert.IsType<StatusCodeResult>(result);
+        Assert.Equal(500, statusCodeResult.StatusCode);
+    }
+
+    
+    [Fact]
+    public async Task UpdateEmployee_ValidRequest_UpdatesEmployeeSuccessfully()
+    {
+        // Arrange
+        var employeeServiceMock = new Mock<IEmployeeService>();
+        var employeeSalaryServiceMock = new Mock<IEmployeeSalaryService>();
+        var updateEmployeeRequest = new UpdateEmployeeRequest
+        {
+            Employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Jane",
+                LastName = "Doe",
+                BasicSalary = 2900000,
+                Allowance = 30000,
+                Transportation = 25000
+            }
+        };
+        employeeServiceMock.Setup(es => es.UpdateEmployee(updateEmployeeRequest))
+            .ReturnsAsync(true);
+
+        var controller = new EmployeeController(employeeServiceMock.Object, employeeSalaryServiceMock.Object);
+
+        // Act
+        var result = await controller.Update(updateEmployeeRequest);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var response = Assert.IsType<ResponseBase<bool>>(okResult.Value);
+        Assert.True(response.IsSuccess);
+        Assert.True(response.Data);
+    }
+
 
 }
